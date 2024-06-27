d4e0ad3c3ed5cad1067ac31f8a7ad2a3f24c001f
Andi Razif <andirazif.pub@outlook.my>
2024-06-27 14:21:12 +0800

    Add logging with Serilog

---
+++ src/Core/Injector.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core;
+
+public static class Injector
+{
+    /// <summary>
+    /// Register core dependencies
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <param name="isDevelopment"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddApplicationCore(this IServiceCollection services, IConfiguration configuration, bool isDevelopment)
+    {
+        // Register services
+
+        return services;
+    }
+}
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,27 +1,62 @@
+using Core;
+using Serilog;
+using Serilog.Events;
 using Web.Components;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+  .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+  .Enrich.FromLogContext()
+  .WriteTo.Console()
+  .CreateBootstrapLogger();
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+Log.Information("Starting up!");
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+
+    // Register Logger
+    builder.Services.AddSerilog((s, c) => c
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(s)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
+
+    builder.Services.AddApplicationCore(builder.Configuration, builder.Environment.IsDevelopment());
+
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseSerilogRequestLogging();
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+    app.UseAntiforgery();
+
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception occurred during bootstrapping");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
