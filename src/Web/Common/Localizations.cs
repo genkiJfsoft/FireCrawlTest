117c244e2119e8aa5484a47f483b2a15a73ea798
Andi Razif <andi@jfsoft.com.my>
2024-09-24 15:38:38 +0800

    Remove unused localizations const

--- src/Web/Common/Localizations.cs
+++ src/Web/Common/Localizations.cs
@@ -7,8 +7,6 @@ namespace Web.Common;
 /// </summary>
 internal sealed class Localizations
 {
-    public const string ResourcesPath = "Localization";
-
     public static List<CultureInfo> SupportedCultures => [new("en"), new("ms")];
 
     public static CultureInfo DefaultCulture => SupportedCultures[0];
